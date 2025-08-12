41003d0d73796c4937836fb7de6a91d5b523bcb9
Vikram <vikramprajapat699@gmail.com>
2025-08-12 11:34:15 +0530

    dh

--- Server.js
+++ Server.js
@@ -26,9 +26,19 @@ let Addressschema = require("./model/Address")
 
 
 // connect database
+// const { mongoose } = require("mongoose")
+// const Products = require("./model/Products")
+// mongoose.connect("mongodb+srv://vikramprajapat699:lWsCwg2Kry9S7OX3@cluster0.ofpee48.mongodb.net/").then((res) => {
+//     console.log("mongodb connect")
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+
+// local 
 const { mongoose } = require("mongoose")
 const Products = require("./model/Products")
-mongoose.connect("mongodb+srv://vikramprajapat699:lWsCwg2Kry9S7OX3@cluster0.ofpee48.mongodb.net/").then((res) => {
+mongoose.connect("mongodb://localhost:27017/").then((res) => {
     console.log("mongodb connect")
 }).catch((err) => {
     console.log(err)
